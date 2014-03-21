26cc643e180519053b150969ddeabd78b8572931
Vladimir Stankovic <vladast.dev@gmail.com>
2014-03-21 01:40:30 +0100

    Added defines for AVR002

--- sw/usbdefs.h
+++ sw/usbdefs.h
@@ -17,9 +17,15 @@
 #define AVR001_DEVICE_ID        0xe8, 0x03
 #define AVR001_DEVICE_NAME      'A', 'V', 'R', '0', '0', '1'
 
-#define USB_CFG_VENDOR_ID       AVR001_VENDOR_ID
-#define USB_CFG_VENDOR_NAME     AVR001_VENDOR_NAME
-#define USB_CFG_DEVICE_ID       AVR001_DEVICE_ID
+/* AVR002 project defines from usbconfig file */
+#define AVR002_VENDOR_ID        0xc0, 0x16
+#define AVR002_VENDOR_NAME      VLADAST_VENDOR_NAME
+#define AVR002_DEVICE_ID        0xe8, 0x03
+#define AVR002_DEVICE_NAME      'A', 'V', 'R', '0', '0', '2'
+
+#define USB_CFG_VENDOR_ID       AVR002_VENDOR_ID
+#define USB_CFG_VENDOR_NAME     AVR002_VENDOR_NAME
+#define USB_CFG_DEVICE_ID       AVR002_DEVICE_ID
 #define USB_CFG_DEVICE_NAME     'V', 'l', 'a', 'd', 'a', 's', 't'//AVR001_DEVICE_NAME
 
 #endif // USBDEFS_H
