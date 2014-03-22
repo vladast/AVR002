d2c877db282bafe43eb14bb4f48c9ce679412280
Vladimir Stankovic <vladast.dev@gmail.com>
2014-03-22 23:07:20 +0100

    Defined get and set requests

--- sw/reqdefs.h
+++ sw/reqdefs.h
@@ -1,7 +1,7 @@
 /* Name:    reqdefs.h
- * Project: AVR001:	USB touch-switch stats
+ * Project: AVR002:	Multi-switch event recorder with USB support
  * Author:  Vladimir Stankovic
- * Creation Date: 2014-03-09
+ * Creation Date: 2014-03-22
  * Copyright: N/A
  * License: N/A
  */
@@ -9,10 +9,16 @@
 #ifndef REQDEFS_H
 #define REQDEFS_H
 
-#define REQ_GET_HEADER          3
+#define REQ_GET_HEADER          0
 
-#define REQ_GET_DATA1           4
+#define REQ_GET_DATA1           10
+#define REQ_GET_DATA2           11
+#define REQ_GET_DATA3           12
+#define REQ_GET_DATA4           13
 
-#define REQ_GET_DATA2           5
+#define REQ_SET_DATA1           20
+#define REQ_SET_DATA2           21
+#define REQ_SET_DATA3           22
+#define REQ_SET_DATA4           23
 
 #endif // REQDEFS_H
